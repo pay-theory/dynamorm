8fa733c66e4d7394d84a1522f0cc2d1530ee7f43
Aron Price <aron@paytheory.com>
2026-01-09 21:25:02 -0500

    test(interfaces): cover DynamoDB wrapper methods

--- pkg/interfaces/aws_wrapper_test.go
+++ pkg/interfaces/aws_wrapper_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/credentials"
@@ -44,7 +45,31 @@ func TestDynamoDBClientWrapper_ForwardsCalls(t *testing.T) {
 
 	ctx := context.Background()
 
-	_, err := wrapper.DescribeTable(ctx, &dynamodb.DescribeTableInput{TableName: aws.String("T")})
+	_, err := wrapper.CreateTable(ctx, &dynamodb.CreateTableInput{
+		TableName:   aws.String("T"),
+		BillingMode: types.BillingModePayPerRequest,
+		KeySchema: []types.KeySchemaElement{
+			{AttributeName: aws.String("pk"), KeyType: types.KeyTypeHash},
+		},
+		AttributeDefinitions: []types.AttributeDefinition{
+			{AttributeName: aws.String("pk"), AttributeType: types.ScalarAttributeTypeS},
+		},
+	})
+	require.Error(t, err)
+
+	_, err = wrapper.DescribeTable(ctx, &dynamodb.DescribeTableInput{TableName: aws.String("T")})
+	require.Error(t, err)
+
+	_, err = wrapper.DeleteTable(ctx, &dynamodb.DeleteTableInput{TableName: aws.String("T")})
+	require.Error(t, err)
+
+	_, err = wrapper.UpdateTimeToLive(ctx, &dynamodb.UpdateTimeToLiveInput{
+		TableName: aws.String("T"),
+		TimeToLiveSpecification: &types.TimeToLiveSpecification{
+			AttributeName: aws.String("ttl"),
+			Enabled:       aws.Bool(true),
+		},
+	})
 	require.Error(t, err)
 
 	_, err = wrapper.Scan(ctx, &dynamodb.ScanInput{TableName: aws.String("T")})
@@ -65,6 +90,60 @@ func TestDynamoDBClientWrapper_ForwardsCalls(t *testing.T) {
 		},
 	})
 	require.Error(t, err)
+
+	_, err = wrapper.DeleteItem(ctx, &dynamodb.DeleteItemInput{
+		TableName: aws.String("T"),
+		Key: map[string]types.AttributeValue{
+			"pk": &types.AttributeValueMemberS{Value: "1"},
+		},
+	})
+	require.Error(t, err)
+
+	_, err = wrapper.Query(ctx, &dynamodb.QueryInput{
+		TableName: aws.String("T"),
+	})
+	require.Error(t, err)
+
+	_, err = wrapper.UpdateItem(ctx, &dynamodb.UpdateItemInput{
+		TableName: aws.String("T"),
+		Key: map[string]types.AttributeValue{
+			"pk": &types.AttributeValueMemberS{Value: "1"},
+		},
+		UpdateExpression: aws.String("SET #a = :v"),
+		ExpressionAttributeNames: map[string]string{
+			"#a": "a",
+		},
+		ExpressionAttributeValues: map[string]types.AttributeValue{
+			":v": &types.AttributeValueMemberS{Value: "v"},
+		},
+	})
+	require.Error(t, err)
+
+	_, err = wrapper.BatchGetItem(ctx, &dynamodb.BatchGetItemInput{
+		RequestItems: map[string]types.KeysAndAttributes{
+			"T": {
+				Keys: []map[string]types.AttributeValue{
+					{"pk": &types.AttributeValueMemberS{Value: "1"}},
+				},
+			},
+		},
+	})
+	require.Error(t, err)
+
+	_, err = wrapper.BatchWriteItem(ctx, &dynamodb.BatchWriteItemInput{
+		RequestItems: map[string][]types.WriteRequest{
+			"T": {
+				{
+					PutRequest: &types.PutRequest{
+						Item: map[string]types.AttributeValue{
+							"pk": &types.AttributeValueMemberS{Value: "1"},
+						},
+					},
+				},
+			},
+		},
+	})
+	require.Error(t, err)
 }
 
 func TestTableWaiterWrapper_Constructors(t *testing.T) {
@@ -75,4 +154,13 @@ func TestTableWaiterWrapper_Constructors(t *testing.T) {
 
 	notExistsWaiter := NewTableNotExistsWaiterWrapper(client)
 	require.NotNil(t, notExistsWaiter)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+
+	err := existsWaiter.Wait(ctx, &dynamodb.DescribeTableInput{TableName: aws.String("T")}, 1*time.Second)
+	require.Error(t, err)
+
+	err = notExistsWaiter.Wait(ctx, &dynamodb.DescribeTableInput{TableName: aws.String("T")}, 1*time.Second)
+	require.Error(t, err)
 }
